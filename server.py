c30ad331f970708baab30e96bd594e7d17743710
Abhishek Gupta <abhishekgupt34@gmail.com>
2017-10-30 15:40:14 +0530

    Added electionTimeout

--- server.py
+++ server.py
@@ -1,5 +1,8 @@
 import zmq
 import sys
+import random
+import ast
+import threading
 
 class LogEntry(Object):
 	def __init__(self, clientId, clientSeqNum, data, term):
@@ -31,8 +34,6 @@ class Log(Object):
 
 
 
-
-
 context = zmq.Context()
 
 
@@ -40,7 +41,8 @@ server_id = int(sys.argv[1])
 router_address = sys.argv[2]
 port_no = sys.argv[3]
 port_no2 = sys.argv[4]
-
+cluster_members = ast.literal_eval(sys.argv[5])
+cluster_members = [int(x) for x in cluster_members]
 sender_socket = context.socket(zmq.PUSH)
 sender_socket.connect(router_address +":"+ port_no)
 
@@ -54,7 +56,45 @@ state = 'f'
 votedFor = None
 lastKnownLeaderID = None
 commitIndex = 0
+maybeNeedToCommit = False
+lastApplied = False
+maxAppliedEntriesInLog = 10000
+hardLogSizeLimit = 100000
+nextIndex = []
+matchIndex = []
+recoveryMode = False
+recoveryPrevLogIndex = 0
+grantedVotes = 0
+election = random.randint(150, 300)
+heartbeatTie = 25
+commitTime = 50
+log = Log(server_id)
 
+electionTimeCall = False
+
+#This function will called every electionTime
+def electionTimeout():
+	if electionTimeCall == True:
+		if lastKnownLeaderID == None:
+			print("No known leader for me, starting election!")
+		else:
+			print("No heartbeat received from leader starting election!")
+		currentTerm += 1
+		state = 'c'
+		votedFor = server_id
+		grantedVotes = 1
+		msg = {'rpc':'requestVote', 'term':currentTerm, 'candidateId':server_id, 
+		'lastLogIndex':log._length-1, 'lastLogTerm':log._entries[-1]._term};
+
+		for destid in clusterMembers:
+			if destid != server_id:
+				msg['dest'] = destid
+				sender_socket.send_json(msg)
+	threading.Timer(1.0, electionTimeout).start()
+
+
+electionTimeout()
+electonTimeCall = True
 
 i = 0
 while True:
